ab1f6da47c9d7dcccafd638c766f133d1cc12b5d
bananatina <101440444+bananatina@users.noreply.github.com>
2023-03-18 22:28:48 -0600

    api

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const multer = require('multer');
+const { exec } = require('child_process');
+
+const app = express();
+const upload = multer({ dest: 'uploads/' });
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://fromzerotoone.net'
+}));
+app.post('/upload', upload.single('file'), (req, res) => {
+  const { path } = req.file;
+  exec(`exiftool -j ${path}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      res.sendStatus(500);
+      return;
+    }
+    const metadata = JSON.parse(stdout)[0];
+    res.json(metadata);
+  });
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
